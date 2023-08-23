7c0d461f1888b2c58e4b0f9c67aad0180b17cd81
makkttub <ariadna.v@outlook.com.ar>
2023-08-23 20:25:20 -0300

    cambio minimo, el separator no hacia falta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,7 @@ const Header = () => {
         <Image src="/logo-final-white.png" className={styles.logo} width={200} height={200} />
         <Image src="/VectorFlecha.png" className={styles.logoFlecha} width={66} height={99} />
         {/* Agrega una separación entre las imágenes */}
-        <div className={styles.separator1} />
-
+      
         {/* Centra el título */}
         <div className={styles.centeredTitle}>
           <Image src="/TITULO.png" className={styles.logo} width={640} height={140} />
