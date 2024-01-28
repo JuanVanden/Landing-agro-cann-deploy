201ab1174b3bfca5c5e93b8ea1d82705f943ae66
JuanVanden <Juanvanden@gmail.com>
2024-01-28 13:21:02 -0300

    Actualizar configuraciones y realizar nuevo build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-
 const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias['@component'] = path.join(__dirname, 'src');
+    config.resolve.alias['~component'] = path.join(__dirname, 'src'); // Modificación aquí
     config.resolve.fallback = {
       ...config.resolve.fallback,
       "fs": false,
