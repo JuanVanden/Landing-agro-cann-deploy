e3e0b9dfb28d4079f4405036fa1eff234538b07d
JuanVanden <Juanvanden@gmail.com>
2024-01-28 12:29:34 -0300

    Modificacion en next.config.js y package.json para deploy web

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
+
 const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias['@component'] = path.join(__dirname, 'src/*');
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        "fs": false,
-        "tls": false,
-        "net": false,
-        "dns": false,
-      },
+    config.resolve.alias['@component'] = path.join(__dirname, 'src');
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "dns": false,
     };
     return config;
   },
